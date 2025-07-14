80e94a81c617c84b181ebf11c2567af304bf8d46
Sofyan Egi <sofyanegil@gmail.com>
2025-07-14 12:14:04 +0700

    feat(customers): implement customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Page() {
-  return <div>Customers Page</div>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+
+  const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental',
   },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
 export default nextConfig;
