405699e3b2ecba9e2660e6a9183bacba5d33dae8
Sofyan Egi <sofyanegil@gmail.com>
2025-07-18 18:22:34 +0700

    fix(token): update use secure cookie

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET! });
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET!, secureCookie: true });
+  console.log(token);
+
   const isAuthenticated = !!token && (!token.exp || token.exp > Math.floor(Date.now() / 1000));
 
   const protectedRoutes = ['/dashboard'];
