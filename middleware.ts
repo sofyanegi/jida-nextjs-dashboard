62896f926eaefcf716b0f0d1258025d6900da3a3
Sofyan Egi <sofyanegil@gmail.com>
2025-07-18 18:22:46 +0700

    fix(token): use cookie key

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET!, secureCookie: true });
-  console.log(token);
+  const cookieKey = process.env.NODE_ENV === 'production' ? '__Secure-authjs.session-token' : 'authjs.session-token';
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    salt: cookieKey,
+    cookieName: cookieKey,
+  });
 
   const isAuthenticated = !!token && (!token.exp || token.exp > Math.floor(Date.now() / 1000));
 
